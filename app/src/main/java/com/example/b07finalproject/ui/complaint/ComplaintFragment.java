780102726a8e5f5ae31efdc90bb62abdda3b89ab
Sanvi Dubey <sanvi.dubey@mail.utoronto.ca>
2023-11-15 13:07:45 -0500

    add complaints

---
+++ app/src/main/java/com/example/b07finalproject/ui/complaint/ComplaintFragment.java
@@ -0,0 +1,38 @@
+package com.example.b07finalproject.ui.complaint;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.b07finalproject.R;
+
+public class ComplaintFragment extends Fragment {
+
+    private ComplaintViewModel mViewModel;
+
+    public static ComplaintFragment newInstance() {
+        return new ComplaintFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_complaint, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ComplaintViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
