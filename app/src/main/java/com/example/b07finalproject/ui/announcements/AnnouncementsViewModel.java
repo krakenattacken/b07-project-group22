b4b2267771e9da6f9cf8d2ab542756e0fc611687
KatH121 <144845272+KatH121@users.noreply.github.com>
2023-12-06 16:45:50 -0500

    Update AnnouncementsViewModel.java

--- app/src/main/java/com/example/b07finalproject/ui/announcements/AnnouncementsViewModel.java
+++ app/src/main/java/com/example/b07finalproject/ui/announcements/AnnouncementsViewModel.java
@@ -9,15 +9,13 @@ import java.util.List;
 public class AnnouncementsViewModel extends ViewModel {
     //data for layout checking purposes; will be deleted
     MutableLiveData<List<Announcement>> announList;
-
-
     List<Announcement> x;
     Announcement e;
 
     public LiveData<List<Announcement>> getItemList() {
         if (announList == null) {
             announList = new MutableLiveData<>();
-            x = e.createAnnouncementList();
+            x = e.createAnnouncementList(1);
             announList.setValue(x);
         }
         return announList;
