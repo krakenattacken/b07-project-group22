b9e0d0e3988fa366eb15f8b6e08e64b7ef75c0b9
KatH121 <144845272+KatH121@users.noreply.github.com>
2023-12-06 16:42:40 -0500

    Update Announcement.java

--- app/src/main/java/com/example/b07finalproject/ui/announcements/Announcement.java
+++ app/src/main/java/com/example/b07finalproject/ui/announcements/Announcement.java
@@ -20,39 +20,11 @@ public class Announcement extends Occasion implements Serializable {
     //methods to be added as needed
 
     //hashcode of username and title (for unique ID)
-
-
+    
 
     //to do when firebase db gets made (for now, just a tester)
-    public static List<Announcement> createAnnouncementList() {
+    public static List<Announcement> createAnnouncementList(int numAnnoun) {
         List<Announcement> announcements = new ArrayList<Announcement>();
-        /*
-        LocalDateTime now = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            now = LocalDateTime.now();
-        }
-        Announcement announ1 = new Announcement("Announ 1", "manually entered time", "Location 1", "Description 1");
-
-        announcements.add(announ1);
-
-        Announcement announ2 = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            announ2 = new Announcement("Announ 2", "manually entered time", "Location 2", "Description 2");
-        }
-        Announcement announ3 = new Announcement("Announ 1", "manually entered time", "Location 1", "Description 1");
-        Announcement announ4 = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            announ4 = new Announcement("Announ 2", "manually entered time", "Location 2", "Description 2");
-        }
-        Announcement announ5 = new Announcement("Announ 1", "manually entered time", "Location 1", "Description 1");
-        announcements.add(announ1);
-
-        announcements.add(announ2);
-        announcements.add(announ3);
-        announcements.add(announ4);
-        announcements.add(announ5);
-
-         */
         return announcements;
     }
 
